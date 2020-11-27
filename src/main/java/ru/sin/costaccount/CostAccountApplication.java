c673902b12809913c949fbbb991b1d8722d3e249
sindenis <cdenissin@gmail.com>
2020-11-28 02:16:39 +0300

    Инициализировал проект

---
+++ src/main/java/ru/sin/costaccount/CostAccountApplication.java
@@ -0,0 +1,13 @@
+package ru.sin.costaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CostAccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CostAccountApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/sin/costaccount/CostAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sin.costaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CostAccountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
